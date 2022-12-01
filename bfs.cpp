0abc94ab30d618493c92a2e4d9c607ee983efc40
FadzararAI <89302350+FadzararAI@users.noreply.github.com>
2022-12-01 22:38:07 +0800

    created bfs algorithm

--- bfs.cpp
+++ bfs.cpp
@@ -1,1 +1,46 @@
+class Graph{
+	int numVertices;
+	vector< vector<int> > adjList;
+	
+	public:
+	//graph constructor
+	Graph(int numVertices);
+	//graph functions
+	void addEdge(int u, int v);
+	void BFS(int src);
+};
 
+//constructor
+Graph::Graph(int numVertices){
+	this->numVertices = numVertices;
+	adjList.resize(numVertices);
+}
+void Graph::BFS(int src){
+	//initialize all nodes as unvisited
+	vector<bool> visited(numVertices, false);
+	
+	//initialize queue for bfs
+	queue<int> q;
+	
+	//mark src node as visited and push to queue
+	visited[src] = true;
+	q.push(src);
+	
+	//loop until queue is empty
+	while(!q.empty()){
+		int temp = q.front();
+		//print the visited nodes
+		cout << "Visited: " << temp << endl;
+		q.pop();
+		
+		//traverse adjlist
+		for(int i = 0; i < adjList[temp].size(); i++){
+			int v = adjList[temp][i];
+			//if not yet visited
+			if(!visited[v]){
+				visited[v] = true;
+				q.push(v);
+			}
+		}
+	}
+}
