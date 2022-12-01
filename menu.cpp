1f91f2d9b5397d35f8c32534c26c3e4d25d2a22b
AminHafidz <115087647+AminHafidz@users.noreply.github.com>
2022-12-01 22:08:45 +0700

    Update menu.cpp
    Add if-else statement to perform the commands

--- menu.cpp
+++ menu.cpp
@@ -5,4 +5,21 @@ void menu(){
 		cout << "Input number of vertices: ";
 		int vertices;
 		cin >> vertices;
+		while(!reset){
+			cout << "1. Insert adjacency list " << endl;
+			cout << "2. View adjacency lists" << endl;
+			cout << "3. Do BFS Traversal" << endl;
+			cout << "4. Reset Graph" << endl;
+			cout << "5. Exit Program" << endl;
+			
+			int chc;
+			cin >> chc;
+			if(chc == 1){}
+			else if(chc == 2){}
+			else if(chc == 3){}
+			else if(chc == 4){}
+			else if(chc == 5){}
+		}
+			
 	}
+}
